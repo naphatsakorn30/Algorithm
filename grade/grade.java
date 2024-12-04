46902052f1d6ffa3ee4674340e5246354c382b79
naphatsakorn30 <naphatsakorn1712@gmail.com>
2024-12-04 12:51:05 +0700

    ส่งงาน

---
+++ grade/grade.java
@@ -0,0 +1,10 @@
+package  grade ;
+import java.util.Scanner;
+
+public class grade {
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in); 
+    System.out.print("  score : ");  
+    }
+    
+}
